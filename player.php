9c88151103104259b6cd0f3e2a1283e9f3d7ecea
Infinit TV <yassine.znasni@gmail.com>
2019-10-21 18:59:20 +0100

    Update player.php

--- player.php
+++ player.php
@@ -6,9 +6,11 @@ if (!isset($_GET["id"])) { exit; } ?>
 <html>
     
     <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+	<script src="assets/js/vendor.min.js"></script>
     <body>
         <video id="video" width="100%" height="100%" controls></video>
     </body>
+	<script src="assets/js/vendor.min.js"></script>
     <script>
     $(document).ready(function() {
         var video = document.getElementById('video');
